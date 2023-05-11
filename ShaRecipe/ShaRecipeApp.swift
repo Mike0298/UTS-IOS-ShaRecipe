b6fe54d977cad8788ea6fa84377d73462484d66a
Mike Pham <mike@veridooh.com>
2023-05-12 09:51:51 +1000

    Initial Commit

---
+++ ShaRecipe/ShaRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShaRecipeApp.swift
+//  ShaRecipe
+//
+//  Created by Mike on 12/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShaRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
